2c6b07a833170366baae120f246921ed7ece1ada
Pavitra Vasudevan <72784548+PaviVasudev@users.noreply.github.com>
2026-08-27 17:18:13 +0530

    Add exercise instructions for Ozone data visualization
    Added instructions for creating bar graphs, histograms, and pie charts to visualize Ozone data.

---
+++ Exercise4.R
@@ -0,0 +1,33 @@
+############################################################
+# EXERCISE 4
+#############################################################
+
+
+# ----------------------------------------------------------
+# BAR GRAPH
+# ----------------------------------------------------------
+
+# 1. Create a bar graph showing the average Ozone for each month.
+# 2. Add an appropriate title.
+# 3. Add appropriate axis labels.
+# 4. Change the appearance of the bars.
+# 5. What does the height of each bar represent?
+
+# ----------------------------------------------------------
+# HISTOGRAM
+# ----------------------------------------------------------
+
+# 6. Create a histogram showing the distribution of Ozone.
+# 7. Add an appropriate title.
+# 8. Change the number of bins.
+# 9. Try different bin widths.
+# 10. Which version communicates the distribution better? Why?
+
+# ----------------------------------------------------------
+# PIE CHART
+# ----------------------------------------------------------
+# 11. Create a frequency table showing observations for each month.
+# 12. Convert the frequency table into a suitable data frame for plotting.
+# 13. Create a pie chart showing the proportion of observations from each month.
+# 14. Add appropriate labels.
+# 15. Is a pie chart the best way to communicate this information? What alternative visualization could you use?
